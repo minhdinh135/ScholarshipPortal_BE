a838960cc6e8ba30c53b0c764cbb841bcf1a5c14
hoanglam1811 <nguyenhoanglam18112003@gmail.com>
2024-10-03 00:03:57 +0700

    add API for Review, Feedback, Document, Award, Applicant Profile

---
+++ Domain/DTOs/AwardDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs
+{
+	public class AwardDTO
+	{
+		public string? Description { get; set; }
+		public decimal? Amount { get; set; }
+		public string? Image { get; set; }
+		public DateTime? AwardedDate { get; set; }
+		public int? ApplicationId { get; set; }
+	}
+}
