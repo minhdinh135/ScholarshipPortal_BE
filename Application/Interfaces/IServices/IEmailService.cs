9b6eee8b0f81e465701cccce821f36b2304d86ac
hoanglam1811 <nguyenhoanglam18112003@gmail.com>
2024-10-09 20:27:39 +0700

    email sending done

---
+++ Application/Interfaces/IServices/IEmailService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.IServices
+{
+	public interface IEmailService
+	{
+		Task SendEmailAsync(string email, string subject, string message, IFormFile? file = null);
+
+	}
+	
+
+}
