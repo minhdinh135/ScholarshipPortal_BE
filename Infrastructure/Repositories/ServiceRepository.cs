1b4e65eb4383d7e971e68d86099fc4527472f11c
Minh Dinh <macotontai123@gmail.com>
2024-11-06 14:35:32 +0700

    feat(service): update Get Service including Feedback and Request Detail

--- Infrastructure/Repositories/ServiceRepository.cs
+++ Infrastructure/Repositories/ServiceRepository.cs
@@ -1,7 +1,7 @@
 ﻿using Application.Interfaces.IRepositories;
-using Domain.Entities;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Service = Domain.Entities.Service;
 
 namespace Infrastructure.Repositories;
 
@@ -11,11 +11,36 @@ public class ServiceRepository : GenericRepository<Service>, IServiceRepository
     {
     }
 
+    public async Task<IEnumerable<Service>> GetAllServices()
+    {
+        var services = await _dbContext.Services
+            .AsNoTracking()
+            .AsSplitQuery()
+            .Include(s => s.Feedbacks)
+            .Include(s => s.RequestDetails)
+            .ToListAsync();
+
+        return services;
+    }
+
+    public async Task<Service> GetServiceById(int id)
+    {
+        var service = await _dbContext.Services
+            .AsSplitQuery()
+            .Include(s => s.Feedbacks)
+            .Include(s => s.RequestDetails)
+            .FirstOrDefaultAsync(s => s.Id == id);
+
+        return service;
+    }
+
     public async Task<IEnumerable<Service>> GetServicesByProviderId(int providerId)
     {
         var services = await _dbContext.Services
             .AsNoTracking()
             .AsSplitQuery()
+            .Include(s => s.Feedbacks)
+            .Include(s => s.RequestDetails)
             .Where(s => s.ProviderId == providerId)
             .ToListAsync();
 
