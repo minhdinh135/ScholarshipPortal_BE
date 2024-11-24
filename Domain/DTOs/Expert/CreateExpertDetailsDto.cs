e3b91f74c7019353263facc95696231ae5479dac
tridominh <tridmse173029@fpt.edu.vn>
2024-11-24 20:18:45 +0700

    change expert post api

--- Domain/DTOs/Expert/CreateExpertDetailsDto.cs
+++ Domain/DTOs/Expert/CreateExpertDetailsDto.cs
@@ -9,4 +9,24 @@ public class CreateExpertDetailsDto
     public string? Major { get; set; }
     
     public int? ExpertId { get; set; }
+
+
+    public string? Email { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? AvatarUrl { get; set; }
+
+    public bool? LoginWithGoogle { get; set; }
+
+    public string? Status { get; set; }
+
+    public int? FunderId { get; set; }
+
+    public int? RoleId { get; set; }
+
 }
