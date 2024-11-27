b6392706d46d58dc78bb8299d0798c4229fd42b4
hoanglam1811 <nguyenhoanglam18112003@gmail.com>
2024-11-27 19:52:41 +0700

    ok

--- Domain/Entities/Subscription.cs
+++ Domain/Entities/Subscription.cs
@@ -3,9 +3,8 @@
 public class Subscription : BaseEntity
 {
     public string? Description { get; set; }
-
-    public decimal? Amount { get; set; } 
     
+    public decimal? Amount { get; set; }
     public int? NumberOfServices { get; set; }
     
     public int? ValidMonths { get; set; }
