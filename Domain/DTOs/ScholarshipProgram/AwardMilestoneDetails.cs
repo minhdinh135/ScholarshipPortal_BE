9d233f4c7b3b783bea6ed8c34a2e4d33451b9bf0
Minh Dinh <macotontai123@gmail.com>
2024-11-27 11:32:50 +0700

    feat(scholarship): update Scholarship Program API with more information
    update Scholarship Program APIs with information including: criteria, review milestones, award
    milestones (award milestone is not included when creating or updating)

---
+++ Domain/DTOs/ScholarshipProgram/AwardMilestoneDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.DTOs.ScholarshipProgram;
+
+public class AwardMilestoneDetails
+{
+    public DateTime? FromDate { get; set; }
+
+    public DateTime? ToDate { get; set; }
+
+    public decimal? Amount { get; set; }
+    
+    public List<string> DocumentTypes { get; set; }
+}
--- Domain/DTOs/ScholarshipProgram/CreateScholarshipProgramRequest.cs
+++ Domain/DTOs/ScholarshipProgram/CreateScholarshipProgramRequest.cs
@@ -2,27 +2,33 @@
 
 public class CreateScholarshipProgramRequest
 {
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
-    public string? ImageUrl { get; set; }
+    public string ImageUrl { get; set; }
 
-    public string? Description { get; set; }
+    public string Description { get; set; }
 
-    public decimal? ScholarshipAmount { get; set; }
+    public decimal ScholarshipAmount { get; set; }
 
-    public int? NumberOfScholarships { get; set; }
-
-    public DateTime? Deadline { get; set; }
-
-    public string? Status { get; set; }
-
-    public int? FunderId { get; set; }
-
-    public int? CategoryId { get; set; }
+    public int NumberOfScholarships { get; set; }
     
-    public int? UniversityId { get; set; }
+    public int NumberOfAwardMilestones { get; set; }
 
-    public int? MajorId { get; set; }
+    public DateTime Deadline { get; set; }
+
+    public string Status { get; set; }
+
+    public int FunderId { get; set; }
+
+    public int CategoryId { get; set; }
     
-    public List<int>? CertificateIds { get; set; }
+    public int UniversityId { get; set; }
+
+    public int MajorId { get; set; }
+    
+    public List<int> CertificateIds { get; set; }
+    
+    public List<CriteriaDetails> Criteria { get; set; }
+    
+    public List<ReviewMilestoneDetails> ReviewMilestones { get; set; }
 }
