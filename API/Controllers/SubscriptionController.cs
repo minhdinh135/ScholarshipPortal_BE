86c71afbabceb202f43fab9bc3b6b8765bce2de8
Minh Dinh <macotontai123@gmail.com>
2024-11-27 10:08:40 +0700

    feat(subscription): add Subscription APIs
    add Get All, Get by Id, Add, Update APIs for Subscription

---
+++ API/Controllers/SubscriptionController.cs
@@ -0,0 +1,72 @@
+﻿using Application.Interfaces.IServices;
+using Domain.DTOs.Common;
+using Domain.DTOs.Subscription;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SSAP.API.Controllers;
+
+[ApiController]
+[Route("api/subscriptions")]
+public class SubscriptionController : ControllerBase
+{
+    private readonly ISubscriptionService _subscriptionService;
+
+    public SubscriptionController(ISubscriptionService subscriptionService)
+    {
+        _subscriptionService = subscriptionService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllSubscriptions()
+    {
+        var allSubscriptions = await _subscriptionService.GetAllSubscriptions();
+
+        return Ok(new ApiResponse(StatusCodes.Status200OK, "Get all subscriptions successfully", allSubscriptions));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetSubscriptionById(int id)
+    {
+        try
+        {
+            var allSubscriptions = await _subscriptionService.GetSubscriptionById(id);
+
+            return Ok(new ApiResponse(StatusCodes.Status200OK, "Get subscription successfully", allSubscriptions));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new ApiResponse(StatusCodes.Status400BadRequest, e.Message));
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddSubscription(AddSubscriptionDto addSubscriptionDto)
+    {
+        try
+        {
+            var result = await _subscriptionService.AddSubscription(addSubscriptionDto);
+
+            return Ok(new ApiResponse(StatusCodes.Status200OK, $"Add subscription with ID: {result} successfully",
+                result));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new ApiResponse(StatusCodes.Status400BadRequest, e.Message));
+        }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateSubscription(int id, UpdateSubscriptionDto updateSubscriptionDto)
+    {
+        try
+        {
+            await _subscriptionService.UpdateSubscription(id, updateSubscriptionDto);
+
+            return Ok(new ApiResponse(StatusCodes.Status200OK, "Update subscription successfully"));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new ApiResponse(StatusCodes.Status400BadRequest, e.Message));
+        }
+    }
+}
