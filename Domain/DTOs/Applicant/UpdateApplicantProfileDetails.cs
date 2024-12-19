10440b606e98286d7725a33abbae8ec68fbd6cdf
Minh Dinh <macotontai123@gmail.com>
2024-12-19 12:52:20 +0700

    fix(applicant-profile): fix error when adding certificates and skills for profile

--- Domain/DTOs/Applicant/UpdateApplicantProfileDetails.cs
+++ Domain/DTOs/Applicant/UpdateApplicantProfileDetails.cs
@@ -2,7 +2,7 @@
 
 public class UpdateApplicantProfileDetails
 {
-    public string AvatarUrl { get; set; }
+    public string? AvatarUrl { get; set; }
     
     public string FirstName { get; set; }
 
@@ -10,29 +10,29 @@ public class UpdateApplicantProfileDetails
 
     public string Username { get; set; }
 
-    public string Phone { get; set; }
+    public string? Phone { get; set; }
 
-    public string Address { get; set; }
+    public string? Address { get; set; }
 
-    public string Gender { get; set; }
+    public string? Gender { get; set; }
 
     public DateTime Birthdate { get; set; }
 
-    public string Nationality { get; set; }
+    public string? Nationality { get; set; }
 
-    public string Ethnicity { get; set; }
+    public string? Ethnicity { get; set; }
     
-    public string Major { get; set; }
+    public string? Major { get; set; }
     
-    public double Gpa { get; set; }
+    public double? Gpa { get; set; }
     
-    public string School { get; set; }
+    public string? School { get; set; }
 
-    public List<string> Achievements { get; set; }
+    public List<string>? Achievements { get; set; }
     
-    public List<string> Skills { get; set; }
+    public List<string>? Skills { get; set; }
 
-    public List<string> Experience { get; set; }
+    public List<string>? Experience { get; set; }
 
-    public List<string> Certificates { get; set; }
+    public List<string>? Certificates { get; set; }
 }
