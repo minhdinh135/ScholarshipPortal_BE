6246304d2bc0748e944b509009fbd033f13f60a6
tridominh <tridmse173029@fpt.edu.vn>
2025-01-16 15:45:52 +0700

    add background process

--- Infrastructure/ExternalServices/Background/BackgroundService.cs
+++ Infrastructure/ExternalServices/Background/BackgroundService.cs
@@ -38,17 +38,56 @@ public class BackgroundService : IBackgroundService
         
         try
         {
-            var openScholarshipPrograms = await _scholarshipProgramRepository.GetOpenScholarshipPrograms();
+            var openScholarshipPrograms = await _scholarshipProgramRepository.GetAll(x => 
+                    x.Include(x => x.AwardMilestones));
 
             foreach (var openScholarshipProgram in openScholarshipPrograms)
             {
-                TimeSpan delayedTimeSpan = (TimeSpan)(openScholarshipProgram.Deadline - DateTime.Now);
-                openScholarshipProgram.Status = ScholarshipProgramStatusEnum.Reviewing.ToString();
+                foreach (var milestone in openScholarshipProgram.AwardMilestones)
+                {
+                    milestone.ScholarshipProgram = null; // Prevent self-referencing loop
+                }
 
-                BackgroundJob.Schedule(() => _scholarshipProgramRepository.Update(openScholarshipProgram),
-                    delayedTimeSpan);
-                _logger.LogInformation(
-                    $"Scholarship program with id:{openScholarshipProgram.Id} is scheduled for updating status after {delayedTimeSpan}");
+                if(openScholarshipProgram.AwardMilestones.Count == 0)
+                    continue;
+                var isAwarding = openScholarshipProgram.AwardMilestones
+                        .Where(x => x.FromDate > DateTime.Now ||
+                            openScholarshipProgram.AwardMilestones
+                            .Any(x => x.FromDate <= DateTime.Now && x.ToDate >= DateTime.Now))
+                        .OrderBy(x => x.FromDate)
+                        .FirstOrDefault();
+                var isCompleted = openScholarshipProgram.AwardMilestones
+                        .OrderByDescending(x => x.FromDate)
+                        .FirstOrDefault();
+                TimeSpan delayedTimeSpan;
+                if(isCompleted != null) {
+                    if(openScholarshipProgram.Status == ScholarshipProgramStatusEnum.Completed.ToString())
+                        continue;
+                    delayedTimeSpan = (TimeSpan)(isCompleted.ToDate - DateTime.Now);
+                    openScholarshipProgram.Status = ScholarshipProgramStatusEnum.Completed.ToString();
+
+                    BackgroundJob.Schedule(() => UpdateScholarshipCompleted(openScholarshipProgram),
+                            delayedTimeSpan);
+                }
+                if(isAwarding != null) {
+                    if(openScholarshipProgram.Status == ScholarshipProgramStatusEnum.Awarding.ToString())
+                        continue;
+                    delayedTimeSpan = (TimeSpan)(isAwarding.FromDate - DateTime.Now);
+                    openScholarshipProgram.Status = ScholarshipProgramStatusEnum.Awarding.ToString();
+
+                    BackgroundJob.Schedule(() => UpdateScholarshipAwarding(openScholarshipProgram),
+                            delayedTimeSpan);
+                }
+                if(openScholarshipProgram.Status == ScholarshipProgramStatusEnum.Reviewing.ToString())
+                        continue;
+                    delayedTimeSpan = (TimeSpan)(openScholarshipProgram.Deadline - DateTime.Now);
+                    openScholarshipProgram.Status = ScholarshipProgramStatusEnum.Reviewing.ToString();
+
+                    BackgroundJob.Schedule(() => UpdateScholarshipReviewing(openScholarshipProgram),
+                        delayedTimeSpan);
+                    _logger.LogInformation(
+                        $"Scholarship program with id:{openScholarshipProgram.Id} is scheduled for updating status after {delayedTimeSpan}");
+
             }
         }
         catch (Exception e)
@@ -74,24 +113,24 @@ public class BackgroundService : IBackgroundService
                 var awardMilestones = await _awardMilestoneRepository.GetAll();
                 awardMilestones = awardMilestones
                     .Where(a => a.ScholarshipProgramId == awardedApplication.ScholarshipProgramId
-                        && a.FromDate > awardedApplication.UpdatedAt
-                        && a.FromDate < DateTime.Now
-                        && a.ToDate > DateTime.Now);
+                        && (a.FromDate > awardedApplication.UpdatedAt ||
+                           (a.FromDate <= DateTime.Now &&
+                            a.ToDate >= DateTime.Now &&
+                            a.FromDate > awardedApplication.UpdatedAt)))
+                    .OrderBy(a => a.FromDate);
 
                 if (awardMilestones.Any())
                 {
                     needExtendApplications.Add((awardedApplication, 
                         awardMilestones.First().FromDate));
                 }
-
             }
             foreach (var needExtendApplication in needExtendApplications)
             {
                 TimeSpan delayedTimeSpan = (TimeSpan)(needExtendApplication.extendDate - DateTime.Now);
                 var application = await _applicationRepository.GetById(needExtendApplication.application.Id);
-                application.Status = ApplicationStatusEnum.NeedExtend.ToString();
 
-                BackgroundJob.Schedule(() => UpdateAndNotifyApplication(application),
+                BackgroundJob.Schedule(() => UpdateAndNotifyApplication(application.Id, ApplicationStatusEnum.NeedExtend.ToString(), needExtendApplication.extendDate),
                     delayedTimeSpan);
                 _logger.LogInformation(
                     $"Application with id:{needExtendApplication.application.Id} is scheduled for updating status after {delayedTimeSpan}");
@@ -100,36 +139,138 @@ public class BackgroundService : IBackgroundService
         catch (Exception e)
         {
             _logger.LogError(e.Message);
-            _logger.LogError(e.InnerException.Message);
+            _logger.LogError(e.StackTrace);
+        }
+    }
+
+    public async Task ScheduleApplicationsRejectedInAward()
+    {
+        _logger.LogInformation("Start Scheduling Applications Need Reject Award");
+        
+        try
+        {
+            var needRejectApplications = await _applicationRepository.GetAll(x => x.Include(x => x.ScholarshipProgram)
+                    .ThenInclude(x => x.AwardMilestones));
+            needRejectApplications = needRejectApplications
+                .Where(a => (a.Status == ApplicationStatusEnum.NeedExtend.ToString() ||
+                            a.Status == ApplicationStatusEnum.Submitted.ToString() ||
+                            a.Status == ApplicationStatusEnum.Approved.ToString()) &&
+                        a.UpdatedAt > a.ScholarshipProgram.Deadline &&
+                        (a.ScholarshipProgram.AwardMilestones.Any(x => x.ToDate > DateTime.Now) || 
+                         a.ScholarshipProgram.AwardMilestones.Any(x => x.ToDate >= DateTime.Now &&
+                             x.FromDate <= DateTime.Now && a.UpdatedAt < x.FromDate
+                )));
+            
+            foreach (var needRejectApplication in needRejectApplications)
+            {
+                var awardMilestone = needRejectApplication.ScholarshipProgram.AwardMilestones
+                    .Where(a => a.ToDate > needRejectApplication.UpdatedAt)
+                    .OrderBy(a => a.FromDate).First();
+                var updateImmediately = needRejectApplication.ScholarshipProgram.AwardMilestones
+                    .Where(a => a.FromDate <= DateTime.Now && a.ToDate >= DateTime.Now &&
+                        needRejectApplication.UpdatedAt < a.FromDate).FirstOrDefault();
+                TimeSpan delayedTimeSpan = (TimeSpan)((updateImmediately != null ?
+                    updateImmediately.ToDate : awardMilestone.ToDate) - DateTime.Now);
+                var application = await _applicationRepository.GetById(needRejectApplication.Id);
+
+                BackgroundJob.Schedule(() => UpdateApplicationRejectedAward(application.Id, ApplicationStatusEnum.Rejected.ToString(),
+                    (updateImmediately != null ? updateImmediately.ToDate : awardMilestone.ToDate)),
+                    delayedTimeSpan);
+                _logger.LogInformation(
+                    $"Application with id:{needRejectApplication.Id} is scheduled for updating status after {delayedTimeSpan}");
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
             _logger.LogError(e.StackTrace);
         }
     }
     
-    public async Task UpdateAndNotifyApplication(Domain.Entities.Application application)
+    public async Task UpdateAndNotifyApplication(int applicationId, string status, DateTime extendDate)
     {
         var awardedApplications = await _applicationRepository.GetAll(x => x.Include(x => x.ScholarshipProgram)
                 .Include(x => x.Applicant));
         awardedApplications = awardedApplications
-            .Where(a => a.Id == application.Id);
+            .Where(a => a.Id == applicationId);
 
         var awardedApplication = awardedApplications.First();
 
-        await _applicationRepository.Update(application);
+        //var application = await _applicationRepository.GetById(applicationId);
+        awardedApplication.Status = status;
+        awardedApplication.UpdatedAt = extendDate.AddDays(1);
+
+        await _applicationRepository.Update(awardedApplication);
 
         await _notificationService.SendNotification(awardedApplication.ApplicantId.ToString(),
                 $"/funder/application/{awardedApplication.Id}",
-                "Need Extend",
-                $"Your application to scholarship {awardedApplication.ScholarshipProgram.Name} is waiting for extend."
+                status,
+                $"Your application to scholarship {awardedApplication.ScholarshipProgram.Name} is {status}."
         );
         try{
-        await _emailService.SendEmailAsync(awardedApplication.Applicant.Email.ToString(),
-                "Need Extend",
-                $"Your application to scholarship {awardedApplication.ScholarshipProgram.Name} is waiting for extend."
-       );}
+            await _emailService.SendEmailAsync(awardedApplication.Applicant.Email.ToString(),
+                    status,
+                    $"Your application to scholarship {awardedApplication.ScholarshipProgram.Name} is {status}."
+           );
+        }
         catch (Exception e){
             _logger.LogError(e.Message);
         }
-
     }
 
+    public async Task UpdateApplicationRejectedAward(int applicationId, string status, DateTime extendDate)
+    {
+        var awardedApplications = await _applicationRepository.GetAll(x => x.Include(x => x.ScholarshipProgram)
+                .ThenInclude(x => x.AwardMilestones)
+                .Include(x => x.Applicant));
+        awardedApplications = awardedApplications
+            .Where(a => a.Id == applicationId);
+
+        var awardedApplication = awardedApplications.First();
+
+        //var application = await _applicationRepository.GetById(applicationId);
+        if(awardedApplication.Status == ApplicationStatusEnum.Awarded.ToString() ||
+          awardedApplication.Status == ApplicationStatusEnum.Rejected.ToString()) 
+            return;
+        awardedApplication.Status = status;
+        awardedApplication.UpdatedAt = extendDate.AddDays(-1);
+
+        await _applicationRepository.Update(awardedApplication);
+
+        await _notificationService.SendNotification(awardedApplication.ApplicantId.ToString(),
+                $"/funder/application/{awardedApplication.Id}",
+                status,
+                $"Your application to scholarship {awardedApplication.ScholarshipProgram.Name} is {status}."
+        );
+        try{
+            await _emailService.SendEmailAsync(awardedApplication.Applicant.Email.ToString(),
+                    status,
+                    $"Your application to scholarship {awardedApplication.ScholarshipProgram.Name} is {status}."
+           );
+        }
+        catch (Exception e){
+            _logger.LogError(e.Message);
+        }        
+    }
+
+    public async Task UpdateScholarshipReviewing(ScholarshipProgram scholarshipProgram)
+    {
+        var scholarship = await _scholarshipProgramRepository.GetById(scholarshipProgram.Id);
+        scholarship.Status = ScholarshipProgramStatusEnum.Reviewing.ToString();
+        await _scholarshipProgramRepository.Update(scholarshipProgram);
+    }
+
+    public async Task UpdateScholarshipAwarding(ScholarshipProgram scholarshipProgram)
+    {
+        var scholarship = await _scholarshipProgramRepository.GetById(scholarshipProgram.Id);
+        scholarship.Status = ScholarshipProgramStatusEnum.Awarding.ToString();
+        await _scholarshipProgramRepository.Update(scholarshipProgram);
+    }
+
+    public async Task UpdateScholarshipCompleted(ScholarshipProgram scholarshipProgram)
+    {
+        var scholarship = await _scholarshipProgramRepository.GetById(scholarshipProgram.Id);
+        scholarship.Status = ScholarshipProgramStatusEnum.Completed.ToString();
+        await _scholarshipProgramRepository.Update(scholarshipProgram);
+    }
 }
